0fc6ceee194e0aba204dcaa674f8c0f45b093869
TurboSquat <tiago2009serra@gmail.com>
2026-07-21 10:55:30 +0100

    Add files via upload

---
+++ teams1998.js
@@ -0,0 +1,66 @@
+const teams1998_data = {
+  'A': { teams: [
+    { name: 'Brazil', players: ['Taffarel', 'Zé Maria', 'Goncalves', 'Júnior Baiano', 'André Cruz', 'Roberto Carlos', 'César Sampaio', 'Flávio Conceição', 'Mauro Silva', 'Denilson', 'Rivaldo', 'Edmundo', 'Ronaldo', 'Romário', 'Bebeto', 'Carlos Germano'] },
+    { name: 'Scotland', players: ['Jim Leighton', 'Tommy Boyd', 'Colin Hendry', 'Colin Calderwood', 'Tosh McKinlay', 'Christian Dailly', 'Craig Burley', 'Paul Lambert', 'Gary McAllister', 'John Collins', 'David Hopkin', 'Ally McCoist', 'Gordon Durie', 'Kevin Gallacher', 'Darren Jackson', 'Andy Goram'] },
+    { name: 'Morocco', players: ['Abdelkader El Brazi', 'Abdelilah Saber', 'Abdelkrim El Hadrioui', 'Noureddine Naybet', 'Youssef Rossi', 'Smahi Triki', 'Lahcen Abrami', 'Mustapha Khalif', 'Karim M\'Ghoghi', 'Youssef Chippo', 'Rachid Azzouzi', 'Abderrahim Ouakili', 'Mustapha Hadji', 'Said Chiba', 'Salaheddine Bassir', 'Abdellatif Arraki'] },
+    { name: 'Norway', players: ['Frode Grodås', 'Alf Inge Håland', 'Henning Berg', 'Ronny Johnsen', 'Stig Inge Bjørnebye', 'Dan Eggen', 'Ståle Solbakken', 'Kjetil Rekdal', 'Petter Rudi', 'Erik Mykland', 'Øyvind Leonhardsen', 'Jostein Flo', 'Frank Strandli', 'Tore André Flo', 'Ole Gunnar Solskjær', 'Thomas Gill'] }
+  ]},
+  'B': { teams: [
+    { name: 'Italy', players: ['Angelo Peruzzi', 'Alessandro Nesta', 'Ciro Ferrara', 'Fabio Cannavaro', 'Alessandro Costacurta', 'Paolo Maldini', 'Angelo Di Livio', 'Demetrio Albertini', 'Dino Baggio', 'Roberto Di Matteo', 'Alessandro Del Piero', 'Fabrizio Ravanelli', 'Christian Vieri', 'Pier Luigi Casiraghi', 'Gianfranco Zola', 'Gianluca Pagliuca'] },
+    { name: 'Chile', players: ['Nelson Tapia', 'Cristián Castañeda', 'Francisco Rojas', 'Javier Margas', 'Pedro Reyes', 'Ronald Fuentes', 'Fernando Cornejo', 'Luis Musrri', 'Marcelo Vega', 'Clarence Acuña', 'José Luis Sierra', 'Pablo Galdames', 'Iván Zamorano', 'Manuel Neira', 'Claudio Núñez', 'Marcelo Ramírez'] },
+    { name: 'Cameroon', players: ['Jacques Songo\'O', 'Rigobert Song', 'Tobie Mimboe', 'Raymond Kalla', 'Ernest Etchi', 'Pierre Womé', 'Romarin Billong', 'Marc-Vivien Foé', 'Joseph-Marie Tchango', 'Fabrice Moreau', 'Cyril Mangan', 'Bernard Tchoutang', 'Patrick M\'Boma', 'Alphonse Tchami', 'Joseph-Désiré Job', 'Vincent Ongandzi'] },
+    { name: 'Austria', players: ['Michael Konsel', 'Anton Pfeffer', 'Wolfgang Feiersinger', 'Peter Schöttel', 'Walter Kogler', 'Roman Mählich', 'Andreas Heraf', 'Dietmar Kühbauer', 'Andreas Herzog', 'Peter Stöger', 'Harald Cerny', 'Heimo Pfeifenberger', 'Dieter Ramusch', 'Ivica Vastić', 'Anton Polster', 'Franz Wohlfahrt'] }
+  ]},
+  'C': { teams: [
+    { name: 'France', players: ['Fabien Barthez', 'Lilian Thuram', 'Laurent Blanc', 'Vincent Candela', 'Marcel Desailly', 'Ibrahim Ba', 'Zinedine Zidane', 'Didier Deschamps', 'Robert Pirès', 'Christian Karembeu', 'Youri Djorkaeff', 'Florian Maurice', 'Stéphane Guivarc\'h', 'David Trezeguet', 'Thierry Henry', 'Lionel Letizi'] },
+    { name: 'South Africa', players: ['Brian Baloyi', 'Sizwe Motaung', 'Willem Jackson', 'Lucas Radebe', 'Mark Fish', 'David Nyathi', 'John Moeti', 'Helman Mkhalele', 'Eric Tinkler', 'John Moshoeu', 'Dumisani Ngobe', 'Theophilus Khumalo', 'Brendan Augustine', 'Philemon Masinga', 'Benni McCarthy', 'André Arendse'] },
+    { name: 'Saudi Arabia', players: ['Mohammed Al-Deayea', 'Mohammed Al-Khilaiwi', 'Hussein Sulimani', 'Abdullah Zebramawi', 'Ahmed Madani', 'Ibrahim Al-Harbi', 'Khalid Al-Muwalid', 'Khamis Al-Zahrani', 'Khalid Al-Temawi', 'Obeid Al-Dosary', 'Youssef Al-Thyniyan', 'Ibrahim Al-Shahrani', 'Fahad Al-Mehallel', 'Sami Al-Jaber', 'Saeed Al-Owairan', 'Hussein Al-Sadiq'] },
+    { name: 'Denmark', players: ['Peter Schmeichel', 'Jacob Laursen', 'Marc Rieper', 'Jes Høgh', 'Jan Heintze', 'Ole Tobiasen', 'Michael Schjønberg', 'Thomas Helveg', 'Claus Thomsen', 'Per Frandsen', 'Allan Nielsen', 'Jon Dahl Tomasson', 'Michael Laudrup', 'Brian Laudrup', 'Miklos Molnar', 'Mogens Krogh'] }
+  ]},
+  'D': { teams: [
+    { name: 'Spain', players: ['Andoni Zubizarreta', 'Albert Ferrer', 'Abelardo Fernández', 'Miguel Ángel Nadal', 'Rafael Alkorta', 'Sergi Barjuán', 'Roberto Ríos', 'Fernando Hierro', 'Josep Guardiola', 'Guillermo Amor', 'Luis Enrique', 'Julen Guerrero', 'Kiko', 'Raúl González', 'Alfonso Pérez', 'José Santiago Cañizares'] },
+    { name: 'Nigeria', players: ['Abiodun Baruwa', 'Taribo West', 'Uche Okechukwu', 'Celestine Babayaro', 'Benedict Iroha', 'Augustine Eguavoen', 'Jay-Jay Okocha', 'Garba Lawal', 'Mutiu Adepoju', 'Finidi George', 'Tijani Babangida', 'Nwankwo Kanu', 'Daniel Amokachi', 'Victor Ikpeba', 'Jonathan Akpoborie', 'Peter Rufai'] },
+    { name: 'Paraguay', players: ['José Luis Chilavert', 'Celso Ayala', 'Catalino Rivarola', 'Carlos Gamarra', 'Francisco Arce', 'Pedro Sarabia', 'Julio César Enciso', 'Roberto Acuña', 'Estanislao Struway', 'Harles Bourdier', 'Miguel Ángel Benítez', 'Hugo Ovelar', 'Derlis Soto', 'Aristides Rojas', 'José Saturnino Cardozo', 'Rubén Ruiz Díaz'] },
+    { name: 'Bulgaria', players: ['Zdravko Zdravkov', 'Trifon Ivanov', 'Radostin Kishishev', 'Georgi Antonov', 'Ivailo Petkov', 'Gosho Ginchev', 'Ivailo Yordanov', 'Zlatko Yankov', 'Yordan Lechkov', 'Krasimir Balakov', 'Daniel Borimirov', 'Ilian Iliev', 'Hristo Stoichkov', 'Emil Kostadinov', 'Lyuboslav Penev', 'Dimitar Popov'] }
+  ]},
+  'E': { teams: [
+    { name: 'Netherlands', players: ['Edwin van der Sar', 'Michael Reiziger', 'Frank de Boer', 'Jaap Stam', 'Winston Bogarde', 'Arthur Numan', 'Clarence Seedorf', 'Ronald de Boer', 'Phillip Cocu', 'Wim Jonk', 'Aron Winter', 'Giovanni van Bronckhorst', 'Patrick Kluivert', 'Dennis Bergkamp', 'Marc Overmars', 'Ed de Goey'] },
+    { name: 'Belgium', players: ['Filip De Wilde', 'Eric Deflandre', 'Eric Van Meir', 'Mike Verstraeten', 'Albert De Roover', 'Gordan Vidović', 'Franky Van der Elst', 'Lorenzo Staelens', 'Enzo Scifo', 'Danny Boffin', 'Nico Van Kerckhoven', 'Marc Wilmots', 'Luc Nilis', 'Luís Oliveira', 'Gilles De Bilde', 'Philippe Vande Walle'] },
+    { name: 'South Korea', players: ['Kim Byung-ji', 'Choi Young-il', 'Hong Myung-bo', 'Lee Min-sung', 'Choi Sung-yong', 'Lee Ki-hyung', 'Yoo Sang-chul', 'Ko Jong-su', 'Lee Sang-yoon', 'Ha Seok-ju', 'Seo Jung-won', 'Ko Jeong-woon', 'Choi Yong-soo', 'Park Kun-ha', 'Kim Do-hoon', 'Seo Dong-myung'] },
+    { name: 'Mexico', players: ['Adolfo Ríos', 'Pável Pardo', 'Duilio Davino', 'Claudio Suárez', 'Joel Sánchez', 'Francisco De Anda', 'Ramón Ramírez', 'Alberto García Aspe', 'Alberto Coyote', 'Braulio Luna', 'Germán Villa', 'Carlos Hermosillo', 'Luis Hernández', 'Luis García', 'Cuauhtémoc Blanco', 'Óscar Pérez'] }
+  ]},
+  'F': { teams: [
+    { name: 'Germany', players: ['Andreas Köpke', 'Thomas Helmer', 'Olaf Thon', 'Jürgen Kohler', 'Matthias Sammer', 'Christian Ziege', 'Michael Tarnat', 'Mario Basler', 'Thomas Häßler', 'Jörg Heinrich', 'Andreas Möller', 'Oliver Bierhoff', 'Ulf Kirsten', 'Jürgen Klinsmann', 'Fredi Bobic', 'Oliver Kahn'] },
+    { name: 'Yugoslavia', players: ['Ivica Kralj', 'Zoran Mirković', 'Goran Đorović', 'Miroslav Đukić', 'Siniša Mihajlović', 'Hristo Vidakovic', 'Slaviša Jokanović', 'Dragan Stojković', 'Vladimir Jugović', 'Dejan Govedarica', 'Albert Nađ', 'Branko Brnović', 'Dejan Savićević', 'Predrag Mijatović', 'Savo Milošević', 'Dragoje Leković'] },
+    { name: 'USA', players: ['Kasey Keller', 'Eddie Pope', 'Alexi Lalas', 'Marcelo Balboa', 'Jeff Agoos', 'Mike Burns', 'Cobi Jones', 'John Harkes', 'Thomas Dooley', 'Claudio Reyna', 'Chris Henderson', 'Roy Wegerle', 'Joe-Max Moore', 'Eric Wynalda', 'Roy Lassiter', 'Brad Friedel'] },
+    { name: 'Iran', players: ['Ahmadreza Abedzadeh', 'Mohammad Khakpour', 'Mohammad Peyravani', 'Reza Shahroudi', 'Mehdi Pashazadeh', 'Naeim Sadavi', 'Ali Modirrousta', 'Dariush Yazdani', 'Hamid Estili', 'Mehrdad Minavand', 'Karim Bagheri', 'Alireza Mansourian', 'Khodadad Azizi', 'Ali Daei', 'Mehdi Mahdavikia', 'Nima Nakisa'] }
+  ]},
+  'G': { teams: [
+    { name: 'Romania', players: ['Bogdan Stelea', 'Dan Petrescu', 'Anton Doboș', 'Liviu Ciubotariu', 'Daniel Prodan', 'Tibor Selymes', 'Gheorghe Popescu', 'Dorinel Munteanu', 'Constantin Gâlcă', 'Gabriel Popescu', 'Gheorghe Hagi', 'Viorel Moldovan', 'Gheorghe Craioveanu', 'Adrian Ilie', 'Marius Lăcătuș', 'Dumitru Stângaciu'] },
+    { name: 'Colombia', players: ['Óscar Córdoba', 'Wilmer Cabrera', 'Jorge Bermúdez', 'Iván Córdoba', 'Luis Moreno', 'Hugo Galeano', 'John Pérez', 'Mauricio Serna', 'Harold Lozano', 'Carlos Valderrama', 'Freddy Rincón', 'Víctor Pacheco', 'Antony de Ávila', 'Faustino Asprilla', 'Víctor Aristizábal', 'Faryd Mondragón'] },
+    { name: 'England', players: ['David Seaman', 'Gary Neville', 'Gareth Southgate', 'Tony Adams', 'Sol Campbell', 'Phil Neville', 'David Batty', 'Paul Ince', 'Rob Lee', 'Steve McManaman', 'Paul Scholes', 'Teddy Sheringham', 'Ian Wright', 'Robbie Fowler', 'Les Ferdinand', 'Nigel Martyn'] },
+    { name: 'Tunisia', players: ['Chokri El Ouaer', 'Khaled Badra', 'Sami Trabelsi', 'Ferid Chouchane', 'Tarek Thabet', 'Taoufik Hichri', 'Soufiane Fekih', 'Riadh Bouazizi', 'Kaies Ghodhbane', 'Zoubeir Beya', 'Skander Souayah', 'Adel Sellimi', 'Hassen Gabsi', 'Mehdi Ben Slimane', 'Riadh Jelassi', 'Ali Boumnijel'] }
+  ]},
+  'H': { teams: [
+    { name: 'Argentina', players: ['Germán Burgos', 'Roberto Ayala', 'Roberto Sensini', 'José Chamot', 'Hernán Díaz', 'Diego Simeone', 'Matías Almeyda', 'Juan Sebastián Verón', 'Leonardo Astrada', 'Juan Román Riquelme', 'Marcelo Gallardo', 'Ariel Ortega', 'Claudio López', 'Hernán Crespo', 'Gabriel Batistuta', 'Pablo Cavallero'] },
+    { name: 'Japan', players: ['Yoshikatsu Kawaguchi', 'Akira Narahashi', 'Yutaka Akita', 'Masami Ihara', 'Norio Omura', 'Naoki Soma', 'Hidetoshi Nakata', 'Motohiro Yamaguchi', 'Tsuyoshi Kitazawa', 'Hiroshi Nanami', 'Masashi Nakayama', 'Shōji Jō', 'Wagner Lopes', 'Kazuyoshi Miura', 'Masayuki Okano', 'Seigo Narazaki'] },
+    { name: 'Croatia', players: ['Dražen Ladić', 'Goran Jurić', 'Dario Šimić', 'Nikola Jerkan', 'Slaven Bilić', 'Zvonimir Soldo', 'Robert Jarni', 'Mario Stanić', 'Robert Prosinečki', 'Zvonimir Boban', 'Aljoša Asanović', 'Alen Bokšić', 'Davor Šuker', 'Goran Vlaović', 'Igor Cvitanović', 'Marijan Mrmić'] },
+    { name: 'Jamaica', players: ['Warren Barrett', 'Frank Sinclair', 'Linval Dixon', 'Durrent Brown', 'Ian Goodison', 'Ricardo Gardner', 'Peter Cargill', 'Fitzroy Simpson', 'Theodore Whitmore', 'Robbie Earle', 'Chris Dawes', 'Stephen Malcolm', 'Paul Hall', 'Deon Burton', 'Marcus Gayle', 'Aaron Lawrence'] }
+  ]}
+};
+
+const teamHasNoTeamPage1998 = {
+  'Morocco': true, 'Chile': true, 'Nigeria': true, 'England': true,
+  'USA': true, 'Iran': true, 'Jamaica': true
+};
+
+const teamBase1998 = {
+  'Brazil': 14, 'Scotland': 32, 'Morocco': 50, 'Norway': 67,
+  'Italy': 85, 'Chile': 103, 'Cameroon': 120, 'Austria': 138,
+  'France': 156, 'South Africa': 174, 'Saudi Arabia': 192, 'Denmark': 210,
+  'Spain': 228, 'Nigeria': 246, 'Paraguay': 263, 'Bulgaria': 281,
+  'Netherlands': 299, 'Belgium': 317, 'South Korea': 335, 'Mexico': 353,
+  'Germany': 371, 'Yugoslavia': 389, 'USA': 407, 'Iran': 417,
+  'Romania': 427, 'Colombia': 445, 'England': 463, 'Tunisia': 480,
+  'Argentina': 498, 'Japan': 516, 'Croatia': 534, 'Jamaica': 552
+};
